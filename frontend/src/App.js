83e8bed3d701edbfc4574fb22790d379d13f0a5b
oimatewtfff <gavrilyuk.vitalya14@mail.ru>
2022-07-01 11:08:15 +0300

    initial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ const App = () => {
                 <Col>
                     <Graphic contracts={contracts} />
                 </Col>
-                <Col md="auto">
+                <Col >
                     <CardTotal total={Total} />
                     <TTable contracts={contracts} />
                 </Col>
